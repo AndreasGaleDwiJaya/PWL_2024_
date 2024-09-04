c289eb5935b7d1bf06c7347d7fd0babd7964f9b0
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-09-04 14:00:50 +0700

    Pertemuan 2 - Praktikum 1 & 2

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+// {
+//     public function hello() {
+//         return 'Hello World';
+//     }
+// }
+
+
+// {
+//     public function index() {
+//         return 'selamat datang';
+//     }
+// }
+
+
+// {
+//    public function about()
+//    {
+//        return 'Nama: [Andreas], NIM: [2241760033]';
+//    }
+// }
+
+
+{
+    public function articles($id)
+    {
+        return "Halaman Artikel dengan ID: {$id}";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,53 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
+Route::get('/selamatdatang', function () {
+    return 'Selamat Datang';
+   });
+
+
+Route::get('/about', function () {
+    return 'Nama : Andreas Gale Dwi Jaya Nim : 2241760033';
+   });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya Andre'.$name;
+    });
+
+Route::get('/posts/{post}/comments/{comment}', function 
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='andre') {
+    return 'Nama saya '.$name;
+});
+
+//Praktikum 2
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/index', [WelcomeController::class,'index']);
+
+Route::get('/about', [WelcomeController::class,'about']);
+
+Route::get('/articles/{id}', [WelcomeController::class,'articles']);
+
